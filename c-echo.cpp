95a268c93abad37503ba4213bd0b3703ea61e52c
jasoncole <mediocrechicken@gmail.com>
2020-01-14 09:13:52 -0800

    made array.sh script that runs a variable number of inputs in a loop

--- c-echo.cpp
+++ c-echo.cpp
@@ -3,7 +3,11 @@
 int main(int argv, char** argc) {
 	// skip first argc index because it's the program
 	for (int i = 1; i < argv; i++) {
-		std::cout << argc[i] << " ";
+		std::cout << argc[i];
+		// print a whitespace after all but hte last iteration
+		if (i < argv -1) {
+			std::cout << " ";
+		}
 	}
 	std::cout << std::endl;
 }
